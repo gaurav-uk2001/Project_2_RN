ecc51002abfbc4f2d9701417e406d2fc95335b84
Gaurav Bisht ⚡️ <gaurav.vaishali2001@gmail.com>
2023-11-28 00:55:14 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+// rnfe
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+      <Text>App</Text>
+      <FlatCards/>
+      </View>
+    </SafeAreaView>
+    
+  )
+}
+
+export default App
