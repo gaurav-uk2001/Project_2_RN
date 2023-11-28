31eb2a7108e3ad6f04333aac25a8134ec9752f88
Gaurav Bisht ⚡️ <gaurav.vaishali2001@gmail.com>
2023-11-28 20:01:50 +0530

    Making beautiful buttons in react native
    Making beautiful buttons and decorating the actioncard

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -53,16 +53,60 @@ view is decreased, dimming it. */}
 }
 
 const styles = StyleSheet.create({
-    headingText: {},
-    card: {},
-    elevatedCard: {},
-    headingContainer: {},
-    headerText: {},
+    
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    card: {
+        width: 350,
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 16
+    },
+    elevatedCard: {
+        backgroundColor: '#E07C24',
+        elevation: 3,
+        shadowOffset: {
+            width: 1,
+            height: 1
+        },
+        shadowColor: '#333',
+        shadowOpacity: 0.4
+    },
+    headingContainer: {
+        height: 40,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: '600'
+    },
     cardImage: {
         height: 190
     },
-    bodyContainer: {},
-    footerContainer: {},
-    socialLinks: {}
+    bodyContainer: {
+        padding: 10
+    },
+    footerContainer: {
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    socialLinks: {
+        fontSize: 16,
+        color: '#000000',
+        backgroundColor: '#FFF',
+        paddingHorizontal: 20,
+        paddingVertical: 6,
+        borderRadius: 6
+        
+    }
 
 })
